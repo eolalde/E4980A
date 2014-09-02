58e8aea050d213f28952269aa6af0d1deed65c16
TCX <eolalde@users.noreply.github.com>
2014-09-02 16:07:47 -0400

    Paths changed for Thermoscience_2, batchfile created

---
+++ LCR_GUI.bat
@@ -0,0 +1,2 @@
+start python C:\Users\Thermoscience_2\Documents\LCR_E4980A\E4980A\lcr_gui\manage.py runserver
+start chrome http://localhost:8000/
--- lcr_gui/manage.py
+++ lcr_gui/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append('C:\\Users\\Eusebio Olalde\\Documents\\E4980A RMT-CTRL\\lcr_com')    
+    #sys.path.append('C:\\Python27\\Lib\\site-packages\\django')
+    sys.path.append('C:\\Users\\Thermoscience_2\\Documents\\LCR_E4980A\\E4980A\\lcr_com\\')    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcr_gui.settings")
 
     from django.core.management import execute_from_command_line
